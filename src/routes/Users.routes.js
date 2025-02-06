4ee723bbc60d2996d4af1d3809ce6333cd1b73b0
luccasPhilotGoncalves <luccas.philot@atos.net>
2025-02-05 22:01:08 -0300

    feat: add middleware para verificar token

--- src/routes/Users.routes.js
+++ src/routes/Users.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const userController = require('../controller/UserController');
+const auth = require("../middleware/auth.middleware");
 const router = express.Router();
 
 router.post('/', userController.createUser);
 router.get('/', userController.getUsers);
 router.get('/:username', userController.getUserByUsername);
-router.delete('/:username', userController.deleteByUsername)
+router.delete('/:username', auth.authMiddleware, userController.deleteByUsername);
 
 module.exports = router;
